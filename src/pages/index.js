7b88fc21fb78573b1fefec3bd2440f93800cb43b
Bilegsaikhan <mr.bilegsaikhan@gmail.com>
2024-09-19 18:21:12 +0800

    ss

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,11 +28,11 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{" "} 
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/pages/index.js
             </code>
-            .
+            . On SAMSUNG Galaxy Note 10 5G
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
